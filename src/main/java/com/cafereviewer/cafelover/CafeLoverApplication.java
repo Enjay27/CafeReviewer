15a7e7a8d7e56d3e808730fc77d8fe7500a1c935
enjay27 <kdkm2002@gmail.com>
2021-03-30 23:20:35 +0900

    first

---
+++ src/main/java/com/cafereviewer/cafelover/CafeLoverApplication.java
@@ -0,0 +1,13 @@
+package com.cafereviewer.cafelover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CafeLoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CafeLoverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cafereviewer/cafelover/CafeLoverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cafereviewer.cafelover;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CafeLoverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
