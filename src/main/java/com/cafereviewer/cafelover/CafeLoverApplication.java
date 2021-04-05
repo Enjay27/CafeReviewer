1d1b3d6b28d80e335986609cbe6a91df76ed932b
enjay27 <kdkm2002@gmail.com>
2021-04-05 23:28:38 +0900

    divide database info

--- src/main/java/com/cafereviewer/cafelover/CafeLoverApplication.java
+++ src/main/java/com/cafereviewer/cafelover/CafeLoverApplication.java
@@ -2,12 +2,20 @@ package com.cafereviewer.cafelover;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
 public class CafeLoverApplication {
 
+	private static final String PROPERTIES =
+			"spring.config.location="
+					+"classpath:/application.yml"
+					+",classpath:/secret.yml";
+
 	public static void main(String[] args) {
-		SpringApplication.run(CafeLoverApplication.class, args);
+		new SpringApplicationBuilder(CafeLoverApplication.class)
+				.properties(PROPERTIES)
+				.run(args);
 	}
 
 }
