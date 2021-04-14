dea32a49f294baf588a429385b99199c8bee3c96
corazon4815 <mango4815@knou.ac.kr>
2021-04-14 16:55:06 +0900

    #refactor : front 폴더구성, Main, Login component 생성

--- cafe-page/src/components/Login/Login.js
+++ cafe-page/src/components/Login/Login.js
@@ -1,41 +1,41 @@
 import React, {Component} from 'react';
-import { Link, Route, Switch, useInput } from 'react-router-dom'
+import { Link, Route, Switch} from 'react-router-dom'
 import {Navbar, Nav, NavDropdown, Button, Jumbotron, Form, FormControl} from 'react-bootstrap';
 import "./Login.css";
 import * as config from '../../config/Config';
 import axios from "axios";
 
-export default Modal = () => {
-  return (
-    <div>
-      <p>The email address or password you entered is incorrect.</p>
-    </div>
-  )
-}
+// export default Modal = () => {
+//   return (
+//     <div>
+//       <p>The email address or password you entered is incorrect.</p>
+//     </div>
+//   )
+// }
 
-const Auth = async (data) => {
-    await axios.post('https://my-json-server.typicode.com/corazon4815/api_test/member',data)
-      .then((result)=>{
-          sessionStorage.setItem("loginId", JSON.stringify(result.id))
-          history.push("/");
-
-
-        //shoes변경([...shoes, ...result.data]);
-        props.more변경(false);
-      })
-      .catch((result)=>{
-        props.alert변경(false);
-        //로딩중이라는 ui 안보이게
-        props.more변경(false);
-        console.log('실패 했어요')
-        console.log(result)
-      })
-}
+// const Auth = async (data) => {
+//     await axios.post('https://my-json-server.typicode.com/corazon4815/api_test/member',data)
+//       .then((result)=>{
+//           sessionStorage.setItem("loginId", JSON.stringify(result.id))
+//           history.push("/");
+//
+//
+//         //shoes변경([...shoes, ...result.data]);
+//         props.more변경(false);
+//       })
+//       .catch((result)=>{
+//         props.alert변경(false);
+//         //로딩중이라는 ui 안보이게
+//         props.more변경(false);
+//         console.log('실패 했어요')
+//         console.log(result)
+//       })
+// }
 
 
 
 function Login() {
-    const email = useInput("");
+    // const email = useInput("");
 
     return (
         <div>
@@ -59,7 +59,7 @@ function Login() {
                   required
                 />
             </Form.Group>
-                <Button variant="danger" type="submit" onSubmit={auth}>Log In</Button>
+                <Button variant="danger" type="submit">Log In</Button>
                 <Form.Text className="text-muted text_align_right">
                     New to Yolp? <a href="/signup">Sign Up</a>
                 </Form.Text>
